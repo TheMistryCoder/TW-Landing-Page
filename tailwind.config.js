d40ed1d3cb60e023f5e246e45d7ee7a86c6e2095
Sanjay Mistry <your-email@example.com>
2025-08-14 17:37:39 +0100

    Work in progress on features section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,28 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: "class",
-  content: [
-  "./index.html",
-  "./**/*.{html,js,ts,jsx,tsx}"
-],
+    darkMode: "class",
+    content: [
+    "./index.html",
+    "./src/**/*.{html,js,ts,jsx,tsx}",
+    "./scripts/**/*.js"
+  ],
   theme: {
     fontFamily: {
       sans: ["Inter", "sans-serif"],
     },
     extend: {
       colors: {
-        solid: "#FCFCFE",
-        surfaceDeep: "#1d232a",
+        // Background colours
+        bgSolidLight: "#FCFCFE",        // Light solid background for sections
+        bgComponentDark: "#1d232a",     // For dark UI components (cards, badges, icon boxes)
+        sectionDarkPrimary: "#1d232a",  // Main dark section background
+        sectionDarkSecondary: "#191e25",// Alternate dark section background
+
+        // Text colours
+        textOnLight: "#1A2930", // Default text colour on light backgrounds
+        textOnDark: "#a6adbb",  // Default text colour on dark backgrounds
+        textMutedOnLight: "#475569cc", // slate-600/80
+        textMutedOnDark: "#d1d5dbcc"  // slate-300/80
       },
     },
   },
